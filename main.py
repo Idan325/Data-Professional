8ec37a16b7eca1e1560b3870b3acce10f997ae54
Idan Yahya <no-reply>
2020-07-11 19:32:37 +0300

    Added proxy option and job links handling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 import selenium.common.exceptions as ex
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import json
+from random import randrange
 
 def filter_search_results():
     # Choose jobs filter parameters
@@ -52,28 +55,50 @@ def filter_search_results():
     ele_job_param = driver.find_element(By.XPATH, "//button[contains(@class,'button--apply')]")
     driver.execute_script("arguments[0].click()", ele_job_param)
 
+def get_text_excluding_children(driver, element):
+    return driver.execute_script("""
+    return jQuery(arguments[0]).contents().filter(function() {
+        return this.nodeType == Node.TEXT_NODE;
+    }).text();
+    """, element)
 
 FILTER_SEARCH = False
-TIMEOUT_SEC = 20
+TIMEOUT_SEC = 1000
 DRIVER_PATH = '/home/idan/Documents/Infinity/Dev/chromedriver'
 URL = "https://linkedin.com"
+PROXY = ""
+JOB_TITLE = "data analyst"
 
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+with open('proxies.json') as proxy_ips_file:
+    ip_addresses = json.load(proxy_ips_file)
+
+    index = randrange(0, len(ip_addresses['proxy_ips']))
+    PROXY = ip_addresses['proxy_ips'][index]
+
+
+chrome_options = webdriver.ChromeOptions()
+
+## Uncomment to use Proxy for request
+#chrome_options.add_argument('--proxy-server=%s' % PROXY)
+driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=chrome_options)
+
+wait = WebDriverWait(driver, TIMEOUT_SEC)
+
 driver.get(URL)
-driver.maximize_window()
+driver.set_window_size(width=810, height=900)
 
-assert "LinkedIn" in driver.title
+wait.until(EC.title_contains, "LinkedIn")
 
 try:
     # Wait for 'Sign in" button and click on it
-    wait = WebDriverWait(driver, TIMEOUT_SEC)
+    
     eleSignInBtn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"a.nav__button-secondary")))
     
     eleSignInBtn.click()
 
-    assert "LinkedIn Login" in driver.title
+    wait.until(EC.title_contains, "LinkedIn Login")
 
-    # Fill in username and password values
+    # Fill in username and password
     actions = ActionChains(driver)
 
     ele_username_input = driver.find_element(By.ID, "username")
@@ -98,7 +123,7 @@ try:
 
     ele_search_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#ember41 > input[class*='search-global']")))
     ele_search_input.clear()
-    ele_search_input.send_keys("data analyst")
+    ele_search_input.send_keys(JOB_TITLE)
 
     ele_search_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'search-global-typeahead')]")))
     ele_search_btn.click()
@@ -109,16 +134,46 @@ try:
     # Get search results as elements list. Extract the header element in each and click,
     # extract data and go back
 
-    # div tag: [@class='search-result__info pt3 pb4 ph0']
-    # a tag with data-control-name 'search_srp_result'
+    actions = ActionChains(driver)
 
-    # list result element: ul tag with 'search-results__list' in class
-    # each result: a tag with a href, click the value (link)
-    #search_results = driver.find_elements(By.XPATH, "//a[@data-control-name='search_srp_result']")
-    search_results = driver.find_elements(By.XPATH, "//*[contains(@class, 'search']")
-    print("Done")
+    wait.until(EC.title_is('"' + JOB_TITLE + '"' + ' | Search | LinkedIn'))
 
-    #for result in search_results:
+    actions.key_down(Keys.PAGE_DOWN)
+    actions.pause(1)
+    actions.key_down(Keys.PAGE_DOWN)
+    actions.pause(1)
+    actions.key_down(Keys.PAGE_DOWN)
+    actions.perform()
+    
+    driver.implicitly_wait(10)
+    search_results = driver.find_elements(By.XPATH, "//div[@class='search-result__image-wrapper']/child::a[1]")
+
+    actions.reset_actions()
+
+    res_file = open("results.txt", 'a')
+
+    actions = ActionChains(driver)
+
+    for result in search_results: 
+        
+        ActionChains(driver) \
+            .key_down(Keys.CONTROL) \
+            .click(result)  \
+            .key_up(Keys.CONTROL)  \
+            .perform()
+
+        driver.switch_to_window(driver.window_handles[1])
+        see_more_btn = wait.until(EC.element_to_be_clickable((By.XPATH, ".//button[@aria-label='See more']")))
+        see_more_btn.click()
+
+        job_details = wait.until(EC.visibility_of_element_located((By.ID, "job-details")))      
+
+        # Print span text_content      
+        print(get_text_excluding_children(driver, job_details)) 
+
+        print("New Tab")
+
+    res_file.close()
 
 except ex.NoSuchElementException as e:
     print("Not found: " + e.msg)
